600a645b9f357185b608ff8bbe97ad1bfc793c04
Adam Daniels <adam@mediadrive.ca>
2017-02-03 16:13:24 -0500

    Add blank? and present? for String/Nil classes

--- lib/blankman.rb
+++ lib/blankman.rb
@@ -1,5 +1,39 @@
 require "blankman/version"
 
 module Blankman
-  # Your code goes here...
+  module StringMethods
+    def blank?
+      strip.empty?
+    end
+
+    def present?
+      !blank?
+    end
+  end
+
+  module NilMethods
+    def blank?
+      true
+    end
+
+    def present?
+      false
+    end
+  end
+
+  def blank?(value)
+    value = value.dup
+    case value
+    when String then value.extend(StringMethods)
+    when NilClass then value.extend(NilMethods)
+    end
+
+    value.blank?
+  end
+  module_function :blank?
+
+  def present?(value)
+    !blank?(value)
+  end
+  module_function :present?
 end
--- test/blankman_test.rb
+++ test/blankman_test.rb
@@ -1,11 +1,15 @@
-require 'test_helper'
+require "test_helper"
 
 class BlankmanTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Blankman::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_blank?
+    assert Blankman.blank?("")
+  end
+
+  def test_present?
+    refute Blankman.present?("")
   end
 end
